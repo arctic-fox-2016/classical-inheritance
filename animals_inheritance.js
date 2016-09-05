13649172fa70e2740477c8f19aee44311d02dfc6
Fadhli Ramadhani Arislan & Peter Raswono <pair+fadhlirams.praswono@hacktiv8.com>
2016-09-05 12:22:10 +0700

    Fadhli Peter

--- animals_inheritance.js
+++ animals_inheritance.js
@@ -1,1 +1,50 @@
 "use strict"
+
+class Animal {
+  constructor() {
+
+  }
+}
+
+class Mammal extends Animal {
+  constructor() {
+    super()
+    this.is_warm_blooded = true
+  }
+}
+
+class Amphibian extends Animal {
+  constructor() {
+    super()
+    this.is_warm_blooded = false
+  }
+}
+
+class Primate extends Mammal {
+  constructor() {
+    super()
+    this.num_legs = 2
+  }
+}
+
+class Frog extends Amphibian {
+  constructor() {
+    super()
+  }
+}
+
+class Bat extends Mammal{
+  constructor() {
+    super()
+  }
+}
+
+class Chimpanzee extends Primate {
+  constructor() {
+    super()
+    this.use_lazer_vision = true
+  }
+}
+
+var monyet = new Chimpanzee()
+console.log(monyet)
